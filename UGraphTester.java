c754b8184f1b3d21384da739341ff9d033f6a860
JoshOrais <58167099+JoshOrais@users.noreply.github.com>
2020-01-24 11:43:06 +0800

    1st Exercise

---
+++ UGraphTester.java
@@ -0,0 +1,13 @@
+public class UGraphTester{
+    public void UGraphTester(){
+        int checker = 0;
+
+        while(checker != 5){
+            System.out.println();
+        }
+    }
+
+    public static void main (String [] args){
+        new UGraphTester();
+    }
+}
