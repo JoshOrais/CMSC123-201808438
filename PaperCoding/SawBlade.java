81d25d8a7afe142d1bdef42427d225da794d7c8e
JoshOrais <michaeljoshuaorais@gmail.com>
2020-02-07 15:12:34 +0800

    PC01_DAY_FINAL

---
+++ PaperCoding/SawBlade.java
@@ -0,0 +1,18 @@
+package PaperCoding;
+
+public class SawBlade{
+    public SawBlade(int x){
+        for(int i=0; i<x; i++){
+            for(int j=0, y=x, z=i; j<=i; j++, y--, z--){
+                for(int k=0; k<y; k++){
+                    if(k<=z){
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
