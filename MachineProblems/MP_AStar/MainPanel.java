15ec0a728c6c648722d84827ff1760cecf2b4380
JoshOrais <michaeljoshuaorais@gmail.com>
2020-04-09 00:00:02 +0800

    updates

---
+++ MachineProblems/MP_AStar/MainPanel.java
@@ -0,0 +1,13 @@
+package MachineProblems.MP_AStar;
+
+import MachineProblems.MP_AStar.DataStructures.*;
+
+public class MainPanel{
+    public MainPanel(){
+        
+    }
+
+    public static void main(String [] args){
+        new MainPanel();
+    }
+}
